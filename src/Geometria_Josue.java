0978de6f1a84879a3ac3e8fa5ad5720ec5298a07
POO <melani.molina@epn.edu.ec>
2022-12-12 12:42:49 -0500

    5

--- src/Geometria_Josue.java
+++ src/Geometria_Josue.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Geometria_Josue{
+
     Scanner sc = new Scanner(System.in);
     private double area;
     private double perimetro;
@@ -40,71 +41,138 @@ public class Geometria_Josue{
 
     private double num1, num2, num3, resultado, pi=3.24;
     public void areacuadrado(){
-        System.out.print("\nIngrese el lado del cuadrado: ");
-        num1 = sc.nextDouble();
-        resultado = num1 * num1;
-        setArea(resultado);
-        System.out.println("\nEl area del cuadrado es: " + getArea());
+           do {
+               System.out.print("\nIngrese el lado del cuadrado: ");
+               num1 = sc.nextDouble();
+               if(num1 < 0){
+                   System.out.println("\nEl dato ingresado es incorrecto ingese nuevamente por favor");
+               }
+           }while(num1 < 0);
+               resultado = num1 * num1;
+               setArea(resultado);
+               System.out.println("\nEl area del cuadrado es: " + getArea());
+
+
     }
     public void arearectangulo(){
-        System.out.print("\nIngrese la base del rectangulo: ");
-        num1 = sc.nextDouble();
-        System.out.print("Ingrese la altura del rectangulo: ");
-        num2 = sc.nextDouble();
+        do {
+            System.out.print("\nIngrese la base del rectangulo: ");
+            num1 = sc.nextDouble();
+            if(num1 < 0){
+                System.out.println("\nEl dato ingresado es incorrecto ingese nuevamente por favor");
+            }
+        }while (num1 < 0);
+        do {
+            System.out.print("Ingrese la altura del rectangulo: ");
+            num2 = sc.nextDouble();
+            if(num2 < 0){
+                System.out.println("\nEl dato ingresado es incorrecto ingese nuevamente por favor");
+            }
+        }while(num2 < 0);
         resultado = num1 * num2;
         setArea(resultado);
         System.out.println("\nEl area del rectangulo es: " + getArea());
     }
     public void areatriangulo(){
+        do {
         System.out.print("\nIngrese la base del triangulo: ");
         num1 = sc.nextDouble();
-        System.out.print("Ingrese la atlura del triangulo: ");
-        num2 = sc.nextDouble();
+            if(num1 < 0){
+                System.out.println("\nEl dato ingresado es incorrecto ingese nuevamente por favor");
+            }
+        }while (num1 < 0);
+        do {
+            System.out.print("Ingrese la atlura del triangulo: ");
+            num2 = sc.nextDouble();
+            if(num2 < 0){
+                System.out.println("\nEl dato ingresado es incorrecto ingese nuevamente por favor");
+            }
+        }while (num2 <0);
         resultado = (num1 * num2) / 2;
         setArea(resultado);
         System.out.println("\nEl area del triangulo es: " + getArea());
     }
     public void areacirculo(){
-        System.out.print("\nIngrese el radio del circulo: ");
-        num1 = sc.nextDouble();
+        do {
+            System.out.print("\nIngrese el radio del circulo: ");
+            num1 = sc.nextDouble();
+            if(num1 < 0){
+                System.out.println("\nEl dato ingresado es incorrecto ingese nuevamente por favor");
+            }
+        }while(num1 < 0);
         resultado = 3.14 * (num1 * num1);
         setArea(resultado);
         System.out.println("\nEl area del circulo es: " + getArea());
     }
     public void perimetrocuadrado(){
-        System.out.print("\nIngrese el lado del cuadrado: ");
-        num1 = sc.nextDouble();
+        do {
+            System.out.print("\nIngrese el lado del cuadrado: ");
+            num1 = sc.nextDouble();
+            if(num1 < 0){
+                System.out.println("\nEl dato ingresado es incorrecto ingese nuevamente por favor");
+            }
+        }while(num1 < 0);
         resultado = num1 + num1 + num1 + num1;
         setPerimetro(resultado);
         System.out.println("El perimetro del cuadrado es: " + getPerimetro());
     }
     public void perimetrorectangulo(){
-        System.out.print("Ingrese la base del rectangulo: ");
-        num1 = sc.nextDouble();
-        System.out.println("Ingrese la altura rectangulo: ");
-        num2 = sc.nextDouble();
+        do {
+            System.out.print("Ingrese la base del rectangulo: ");
+            num1 = sc.nextDouble();
+            if(num1 < 0){
+                System.out.println("\nEl dato ingresado es incorrecto ingese nuevamente por favor");
+            }
+        }while(num1 < 0);
+        do {
+            System.out.println("Ingrese la altura rectangulo: ");
+            num2 = sc.nextDouble();
+            if(num2 < 0){
+                System.out.println("\nEl dato ingresado es incorrecto ingese nuevamente por favor");
+            }
+        }while(num2 < 0);
         resultado = 2 * (num1 + num2);
         setPerimetro(resultado);
         System.out.println("\nEl perimtero del rectangulo es: " + getPerimetro());
 
     }
     public void perimetrotriangulo(){
-        System.out.print("\nIngrese el valor del primer lado: ");
-        num1 = sc.nextDouble();
-        System.out.print("Ingrese el valor del segundo lado: ");
-        num2 = sc.nextDouble();
-        System.out.print("Ingrese el valor del tercer lado: ");
-        num3 = sc.nextDouble();
+        do {
+            System.out.print("\nIngrese el valor del primer lado: ");
+            num1 = sc.nextDouble();
+            if(num1 < 0){
+                System.out.println("\nEl dato ingresado es incorrecto ingese nuevamente por favor");
+            }
+        }while(num1 < 0);
+        do {
+            System.out.print("Ingrese el valor del segundo lado: ");
+            num2 = sc.nextDouble();
+            if(num2 < 0){
+                System.out.println("\nEl dato ingresado es incorrecto ingese nuevamente por favor");
+            }
+        }while(num2 < 0);
+        do {
+            System.out.print("Ingrese el valor del tercer lado: ");
+            num3 = sc.nextDouble();
+            if(num3 < 0){
+                System.out.println("\nEl dato ingresado es incorrecto ingese nuevamente por favor");
+            }
+        }while(num3 < 0);
         resultado = num1 + num2 + num3;
         setPerimetro(resultado);
         System.out.println("\nEl perimetro del triangulo es: " + getPerimetro());
     }
-    public void perimetrocirculo(){
-        System.out.print("\nIngrese el valor del radio: ");
-        num1 = sc.nextDouble();
-        resultado = 2 * 3.14 * num1;
-        setPerimetro(resultado);
-        System.out.println("\nEl perimetro del circulo es: " + getPerimetro());
+    public void perimetrocirculo() {
+        do {
+            System.out.print("\nIngrese el valor del radio: ");
+            num1 = sc.nextDouble();
+            if (num1 < 0) {
+                System.out.println("\nEl dato ingresado es incorrecto ingese nuevamente por favor");
+            }
+        }while (num1 < 0) ;
+            resultado = 2 * 3.14 * num1;
+            setPerimetro(resultado);
+            System.out.println("\nEl perimetro del circulo es: " + getPerimetro());
+        }
     }
 
-}
--- src/Main.java
+++ src/Main.java
@@ -5,119 +5,125 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int opcion, opcion1, opcion2, opcion3, repeticion;
         Geometria_Josue geo = new Geometria_Josue();
-
-        System.out.println("\u001B[32m****MENU PRINCIPAL****");
-        System.out.println("\n1. Areas");
-        System.out.println("2. Perimetros");
-        System.out.println("3. Volumenes");
-        do {
-            System.out.print("\nEscoga el calculo que desea realizar: ");
-            opcion = sc.nextInt();
-        } while (opcion < 1 || opcion > 3);
-        switch (opcion) {
-            case 1:
-                do {
-                    System.out.println("\nOpcion escogida: Areas");
-                    System.out.println("\n\t-/-/-/-/ SUBMENU AREAS -/-/-/-/");
-                    System.out.println("\n1. Area del cuadrado");
-                    System.out.println("2. Area del rectangulo");
-                    System.out.println("3. Area del trianuglo");
-                    System.out.println("4. Area del circulo");
+        try {
+            System.out.println("\u001B[32m****MENU PRINCIPAL****");
+            System.out.println("\n1. Areas");
+            System.out.println("2. Perimetros");
+            System.out.println("3. Volumenes");
+            do {
+                System.out.print("\nEscoga el calculo que desea realizar: ");
+                opcion = sc.nextInt();
+            } while (opcion < 1 || opcion > 3);
+            switch (opcion) {
+                case 1:
                     do {
-                        System.out.print("\nEscoja el area a calcular: ");
-                        opcion1 = sc.nextInt();
-                    } while (opcion1 < 1 || opcion1 > 4);
-                    switch (opcion1) {
+                        System.out.println("\nOpcion escogida: Areas");
+                        System.out.println("\n\t-/-/-/-/ SUBMENU AREAS -/-/-/-/");
+                        System.out.println("\n1. Area del cuadrado");
+                        System.out.println("2. Area del rectangulo");
+                        System.out.println("3. Area del trianuglo");
+                        System.out.println("4. Area del circulo");
+                        do {
+                            System.out.print("\nEscoja el area a calcular: ");
+                            opcion1 = sc.nextInt();
+                        } while (opcion1 < 1 || opcion1 > 4);
+                        switch (opcion1) {
+                            case 1:
+                                System.out.println("\nOpcion escogida: Area del cuadrado");
+                                geo.areacuadrado();
+                                break;
+                            case 2:
+                                System.out.println("\nOpcion escogida: Area del Rectangulo");
+                                geo.arearectangulo();
+                                break;
+                            case 3:
+                                System.out.println("\nOpcion escogida: Area del triangulo");
+                                geo.areatriangulo();
+                                break;
+                            case 4:
+                                System.out.println("\nOpcion escogida: Area del circulo");
+                                geo.areacirculo();
+                                break;
+                        }
+                        System.out.print("\nDesea calcular otra area 1. SI : ");
+                        repeticion = sc.nextInt();
+                        System.out.println();
+                    } while (repeticion == 1);
+
+                    break;
+                case 2:
+                    do {
+                        System.out.println("\nOpcion escogida: Perimetro");
+
+                        System.out.println("\u001B[31m\n****SUBMENU PERIMETRO****");
+                        System.out.println("\n1. Perimetro del cuadrado");
+                        System.out.println("2. Perimetro del rectangulo");
+                        System.out.println("3. Perimetro del trianuglo");
+                        System.out.println("4. Perimetro del circulo");
+                        do {
+                            System.out.print("\nEscoja el perimetro a calcular: ");
+                            opcion2 = sc.nextInt();
+                        } while (opcion2 < 1 || opcion2 > 4);
+                        switch (opcion2) {
+                            case 1:
+                                System.out.println("\nOpcion escogida: Perimetro del cuadrado");
+                                geo.perimetrocuadrado();
+                                break;
+                            case 2:
+                                System.out.println("\nOpcion escogida: Perimetro del rectangulo");
+                                geo.perimetrorectangulo();
+                                break;
+                            case 3:
+                                System.out.println("\nOpcion escogida: Perimetro del triangulo");
+                                geo.perimetrotriangulo();
+                                break;
+                            case 4:
+                                System.out.println("\nOpcion escogida: Perimetro del circulo");
+                                geo.perimetrocirculo();
+                                break;
+                        }
+                        System.out.print("\nDesea calcular otro perimetro 1.Si: ");
+                        repeticion = sc.nextInt();
+                    } while (repeticion == 1);
+
+                    break;
+                case 3:
+                    System.out.println("\nOpcion escogida: Volumenes");
+
+                    System.out.println("\n****SUBMENU VOLUMENES****");
+                    System.out.println("\n1. Volumen del cuadrado");
+                    System.out.println("2. Volumen del rectangulo");
+                    System.out.println("3. Volumen del trianuglo");
+                    System.out.println("4. Volumen del circulo");
+                    do {
+                        System.out.print("\nEscoja el volumen a calcular: ");
+                        opcion3 = sc.nextInt();
+                    } while (opcion3 < 1 || opcion3 > 4);
+                    switch (opcion3) {
                         case 1:
-                            System.out.println("\nOpcion escogida: Area del cuadrado");
-                            geo.areacuadrado();
+                            System.out.println("Opcion escogida: Volumen del cuadrado");
+
                             break;
                         case 2:
-                            System.out.println("\nOpcion escogida: Area del Rectangulo");
-                            geo.arearectangulo();
+                            System.out.println("Opcion escogida: Volumen del rectangulo");
+
                             break;
                         case 3:
-                            System.out.println("\nOpcion escogida: Area del triangulo");
-                            geo.areatriangulo();
+                            System.out.println("Opcion escogida: Volumen del triangulo");
+
                             break;
                         case 4:
-                            System.out.println("\nOpcion escogida: Area del circulo");
-                            geo.areacirculo();
+                            System.out.println("Opcion escogida: Volumen del circulo");
+
                             break;
                     }
-                    System.out.print("\nDesea calcular otra area 1. SI : ");
-                    repeticion = sc.nextInt();
-                    System.out.println();
-                } while (repeticion == 1);
+                    break;
 
-                break;
-            case 2:
-                do {
-                    System.out.println("\nOpcion escogida: Perimetro");
-
-                    System.out.println("\u001B[31m\n****SUBMENU PERIMETRO****");
-                    System.out.println("\n1. Perimetro del cuadrado");
-                    System.out.println("2. Perimetro del rectangulo");
-                    System.out.println("3. Perimetro del trianuglo");
-                    System.out.println("4. Perimetro del circulo");
-                    do {
-                        System.out.print("\nEscoja el perimetro a calcular: ");
-                        opcion2 = sc.nextInt();
-                    } while (opcion2 < 1 || opcion2 > 4);
-                    switch (opcion2) {
-                        case 1:
-                            System.out.println("\nOpcion escogida: Perimetro del cuadrado");
-                            geo.perimetrocuadrado();
-                            break;
-                        case 2:
-                            System.out.println("\nOpcion escogida: Perimetro del rectangulo");
-                            geo.perimetrorectangulo();
-                            break;
-                        case 3:
-                            System.out.println("\nOpcion escogida: Perimetro del triangulo");
-                            geo.perimetrotriangulo();
-                            break;
-                        case 4:
-                            System.out.println("\nOpcion escogida: Perimetro del circulo");
-                            geo.perimetrocirculo();
-                            break;
-                    }
-                    System.out.print("\nDesea calcular otro perimetro 1.Si: ");
-                    repeticion = sc.nextInt();
-                }while(repeticion == 1);
-
-                break;
-            case 3:
-                System.out.println("\nOpcion escogida: Volumenes");
-
-                System.out.println("\n****SUBMENU VOLUMENES****");
-                System.out.println("\n1. Volumen del cuadrado");
-                System.out.println("2. Volumen del rectangulo");
-                System.out.println("3. Volumen del trianuglo");
-                System.out.println("4. Volumen del circulo");
-                do {
-                    System.out.print("\nEscoja el volumen a calcular: ");
-                    opcion3 = sc.nextInt();
-                } while (opcion3 < 1 || opcion3 > 4);
-                switch (opcion3) {
-                    case 1:
-                        System.out.println("Opcion escogida: Volumen del cuadrado");
-
-                        break;
-                    case 2:
-                        System.out.println("Opcion escogida: Volumen del rectangulo");
-
-                        break;
-                    case 3:
-                        System.out.println("Opcion escogida: Volumen del triangulo");
-
-                        break;
-                    case 4:
-                        System.out.println("Opcion escogida: Volumen del circulo");
-
-                        break;
-                }
-                break;
+            }
+        } catch (Exception e) {
+            System.out.println("Datos Ingresados son incorrectos");
+        }finally {
+            System.out.println("El programa ha Finalizado con exito !!");
         }
     }
 }
